053690c4738189e5648b674f2ba1919a507fe25d
BheemaGunaShekar <144918504+BheemaGunaShekar@users.noreply.github.com>
2023-09-13 18:55:07 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,94 @@
+let allMusic = 
+[    {
+        name: "Nara nara",
+        artist: "Silambarasan,Deepthi suresh",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Gali valuga",
+        artist: "Anirudh Ravichandh",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Massu Maranam",
+        artist: "Anirudh Ravichandh",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Pedhavi Chivarike",
+        artist: "Vijay prakash",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Dakko Dhaako Meka",
+        artist: "Shivam",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Shivoham",
+        artist: "Haricharan",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Ayila Ayila",
+        artist: "Haricharan & Natalie Di Luccio",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Naa ready",
+        artist: "Vijay,Anirudh Ravichandh",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Bloody Sweet",
+        artist: "Anirudh Ravichandh",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Jai Jai Ganesh",
+        artist: "S.P. Balasubhramanyam",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name:"Not Ramaiah",
+        artist:"Anirudh",
+        img:"music-11",
+        src:"music-11"
+    },
+    {
+        name:"Chalona",
+        artist:"Adhithya Rk,Priya",
+        img:"music-12",
+        src:"music-12",
+    },
+    {
+        name:"Bad-girlfriend",
+        artist:"Anne marie",
+        img:"music-13",
+        src:"music-13",
+
+    },
+    {
+        name:"Friends",
+        artist:"Anne marie",
+        img:"music-14",
+        src:"music-14",
+    },
+    {
+        name:"Ciao_Adios",
+        artist:"Anne marie",
+        img:"music-15",
+        src:"music-15",
+    }
+
+];
